21e704aa71d39f08a25cd2cfb7c199ad3e265947
Cheolone <cherner_oleg@mail.ru>
2018-07-13 01:10:48 +0300

    Added Security model Spring realisation.

---
+++ SecurityModel/src/main/java/com/secure/entity/enums/UserRoleEnum.java
@@ -0,0 +1,12 @@
+package com.secure.entity.enums;
+
+public enum UserRoleEnum {
+
+    ADMIN,
+    USER,
+    ANONYMOUS;
+
+    UserRoleEnum() {
+    }
+
+}
---
+++ SecurityModel/src/main/java/com/secure/service/UserService.java
@@ -0,0 +1,9 @@
+package com.secure.service;
+
+import com.secure.entity.User;
+
+public interface UserService {
+
+    User getUser(String login);
+
+}
